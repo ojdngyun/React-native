2e4eff6bd36f201bd0e7870a8f21202c246ed0fb
oliver <oliverng1602@gmail.com>
2017-02-19 22:25:58 -0500

    [GithubBrowser] Added ViewPager

--- GithubBrowser/AppContainer.js
+++ GithubBrowser/AppContainer.js
@@ -10,8 +10,12 @@ import {
  TextInput,
  TouchableHighlight,
  ActivityIndicator,
- TabBar
+ TouchableOpacity,
+ TouchableNativeFeedback
 } from 'react-native';
+
+var ScrollableTabView = require('react-native-scrollable-tab-view');
+var DefaultTab = require('./DefaultTab');
 
 class AppContainer extends Component{
   constructor(props){
@@ -19,16 +23,45 @@ class AppContainer extends Component{
     this.state = {
       selectedTab: 'feed'
     }
+
+    this.onPress = this.onPress.bind(this);
   }
 
   render(){
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Tabs coming soon!
-        </Text>
-      </View>
+      // <View style={styles.container}>
+      //   <Text style={styles.welcome}>
+      //   </Text>
+      // </View>
+      <ScrollableTabView
+        style={styles.container}
+        renderTabBar={() => <DefaultTab />}
+        ref={(tabView) => {this.tabView = tabView;}}>
+        <Text tabLabel='Tab #1'
+          style={styles.text}>page one</Text>
+        <Text tabLabel='Tab #2'
+          style={styles.text}>page two</Text>
+        <Text tabLabel='Tab #3'
+          style={styles.text}>page three</Text>
+        <TouchableNativeFeedback tabLabel='Button'
+          background={TouchableNativeFeedback.SelectableBackground()}
+          onPress={this.onPress(0)}>
+          <View
+            style={{width: 150, height: 100, backgroundColor: 'gray'}}>
+            <Image style={{height: 50, width: 50}}
+              source={require('./Octocat.png')}/>
+          </View>
+        </TouchableNativeFeedback>
+        <TouchableOpacity tabLabel='Back' onPress={() => this.tabView.goToPage(0)}>
+          <Image style={{height: 50, width: 50}}
+            source={require('./Octocat.png')}/>
+        </TouchableOpacity>
+      </ScrollableTabView>
     );
+  }
+
+  onPress(value){
+    console.log(value);
   }
 }
 
@@ -49,6 +82,17 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+  page: {
+    flex: 0.1,
+    flexDirection: 'column',
+    backgroundColor: 'red'
+  },
+  text: {
+    flex: 1,
+    fontSize: 25,
+    color: 'red',
+    backgroundColor: 'black'
+  }
 });
 
 module.exports = AppContainer;
